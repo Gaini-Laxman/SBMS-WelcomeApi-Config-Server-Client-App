08edad34bfcb94a1aa427fde5fcab1ed81040410
Gaini Laxman <gainilaxman12@gmail.com>
2023-05-22 20:00:20 +0530

    SBMS-WelcomeApi-Config-Server-Client-App

---
+++ src/main/java/com/klinnovations/rest/WelcomeRestController.java
@@ -0,0 +1,19 @@
+package com.klinnovations.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@Value("${msg}")
+	private String msg;
+
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		return msg;
+	}
+	
+
+}
